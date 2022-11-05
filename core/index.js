1da0a616711c0d47430fd2ca6b37986bbed14b71
tao <taowuu@foxmail.com>
2022-11-05 08:56:31 +0800

    第二步完成

--- core/index.js
+++ core/index.js
@@ -1,3 +1,14 @@
 // 打包程序
 
+const fs = require("fs");
 
+function createAsset() {
+  // 1. 获取文件的内容
+  let source = fs.readFileSync('../main.js', "utf-8");
+  console.log('mian.js 文件内容: ', source)
+  
+  // 
+  return {}
+}
+
+createAsset()
